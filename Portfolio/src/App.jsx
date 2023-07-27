517e7ee8655567d59cd8a177e8650e1c06a3bb89
ConstantMotion1 <constan.fernando@hotmail.com>
2023-07-26 21:17:17 -0400

    addd metallic palette for background

---
+++ Portfolio/src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+import './Test.css'
+
+function App() {
+  
+
+  return (
+    <>
+    <div class="solar-system">
+    <div class="sun"></div>
+    <div class="planet mercury"></div>
+    <div class="planet venus"></div>
+    <div class="planet earth"></div>
+    <div class="planet mars"></div>
+    <div class="planet jupiter"></div>
+    <div class="planet saturn"></div>
+    <div class="planet uranus"></div>
+    <div class="planet neptune"></div>
+</div>
+
+    </>
+  )
+}
+
+export default App
